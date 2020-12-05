9b7a0ec16d230fc9f6de51ea8386a96a348534a4
Mohammed Saleh Baber <31297109+mohammed123saleh@users.noreply.github.com>
2020-12-05 06:23:45 +0200

    Update settings.py

--- Travencia/settings.py
+++ Travencia/settings.py
@@ -224,8 +224,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'a.ahmed@egyptfabrics.com'
-EMAIL_HOST_PASSWORD = 'Ms975899'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
